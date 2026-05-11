78d34120fb00a7b90e0f060c3a6518ac3e035bce
Lmacconnell2 <mac23068@byui.edu>
2026-05-11 09:24:36 -0600

    added pages to menu

--- src/views/admin/AdminMenu.php
+++ src/views/admin/AdminMenu.php
@@ -2,11 +2,14 @@
 namespace SweetDesk\Admin;
 
 class AdminMenu {
+
     public function __construct() {
         add_action('admin_menu', [$this, 'registerMenu']);
     }
 
     public function registerMenu() {
+
+        // Main Menu
         add_menu_page(
             'SweetDesk',
             'SweetDesk',
@@ -17,6 +20,7 @@ class AdminMenu {
             6
         );
 
+        // Tickets
         add_submenu_page(
             'sweetdesk',
             'Tickets',
@@ -26,6 +30,7 @@ class AdminMenu {
             [$this, 'ticketsPage']
         );
 
+        // Analytics
         add_submenu_page(
             'sweetdesk',
             'Analytics',
@@ -34,13 +39,55 @@ class AdminMenu {
             'sweetdesk-analytics',
             [$this, 'analyticsPage']
         );
+
+        // Clients
+        add_submenu_page(
+            'sweetdesk',
+            'Clients',
+            'Clients',
+            'manage_options',
+            'sweetdesk-clients',
+            [$this, 'clientsPage']
+        );
+
+        // People
+        add_submenu_page(
+            'sweetdesk',
+            'People',
+            'People',
+            'manage_options',
+            'sweetdesk-people',
+            [$this, 'peoplePage']
+        );
+
+        // Teams
+        add_submenu_page(
+            'sweetdesk',
+            'Teams',
+            'Teams',
+            'manage_options',
+            'sweetdesk-teams',
+            [$this, 'teamsPage']
+        );
     }
 
     public function ticketsPage() {
-        include plugin_dir_path(__FILE__) . '/tickets/tickets-list.php';
+        include plugin_dir_path(__FILE__) . 'tickets/tickets-list.php';
     }
 
     public function analyticsPage() {
-        include plugin_dir_path(__FILE__) . '/tickets/analytics.php';
+        include plugin_dir_path(__FILE__) . 'analytics/analytics.php';
+    }
+
+    public function clientsPage() {
+        include plugin_dir_path(__FILE__) . 'clients/clients.php';
+    }
+
+    public function peoplePage() {
+        include plugin_dir_path(__FILE__) . 'people/people.php';
+    }
+
+    public function teamsPage() {
+        include plugin_dir_path(__FILE__) . 'teams/teams.php';
     }
 }
